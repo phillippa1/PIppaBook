060264d94cafbb0eb105d12f001a274f4c4b1a6d
Pippa Hamilton <phillippa.hamilton@gmail.com>
2025-08-15 16:29:16 +0100

    Part 19- Loops in php

--- connect.php
+++ connect.php
@@ -1,21 +1,11 @@
 <?php 
 
-//part 18- arrays
+//part 19- loops- while loops
 
-//this is an array and in the first location this is in it
-//went over array errors
-$a[0] = 1;
-$a[1] = "eathorne";
-$a[2] = "june";
-//replacing what is in an array
-//$a[2] = "june2";
-//adding to an array on the end
-$a[] = "june3";
+$number = 0;
 
-
-//use a html tag to see the php better
-echo "<pre>";
-print_r($a);
-echo "</pre>";
-
-echo $a[2]; 
+while($number < 20)
+{
+    $number = $number + 1;
+    echo $number . "<br>";
+}
