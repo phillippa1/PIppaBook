ecbe15a315804eec6e7860c98b7195191d0bc56c
Pippa Hamilton <phillippa.hamilton@gmail.com>
2025-08-15 14:04:14 +0100

    Part 17- variables in php

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php 
+
+//varaibles- went over numbers (no need for more) and words (in inverted commas)
+//naming of variables
+//went over working with numbers
+//and the warnings if you try to compute something forbidden
+$number = 2;
+$number2 = 8;
+
+//means show me- tells php this is a word
+echo  $number + $number2;
